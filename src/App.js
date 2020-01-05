3f1c1c58db262b407be074b8b4cd0293e53e24a8
arjun <arjun.ghimire8386@gmail.com>
2020-01-05 21:54:01 +0545

    JSX Demo

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
+ return (
     <div className="App">
       <h2>Hi ! I am React App </h2>
     </div>
   );
+  //return React.createElement('div',{className: 'App'},React.createElement('h1',null,'Does This Works?'))
 }
 export default App;
