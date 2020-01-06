60547bc4c9447f19f6d093bbcfe5b7e7aef083f0
arjun <arjun.ghimire8386@gmail.com>
2020-01-06 20:15:38 +0545

    Create new Person

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
  return (
     <div className="App">
       <h2>Hi ! I am React App </h2>
+      <Person />
     </div>
   );
   //return React.createElement('div',{className: 'App'},React.createElement('h1',null,'Does This Works?'))
