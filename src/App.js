6b92cd048d8f734c00a9c81acfa545eced903cc5
arjun <arjun.ghimire8386@gmail.com>
2020-01-06 20:37:36 +0545

    Props property

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
  return (
     <div className="App">
       <h2>Hi ! I am React App </h2>
-      <Person />
-      <Person />
-      <Person />
+      <Person name="Arjun" age="23"/>
+      <Person name="Ngawang Chokdup Lama" age="27">My Hobbies: Learning,Dancing etc</Person>
+      <Person name="Piyush Pant" age="28"/>
     </div>
   );
   //return React.createElement('div',{className: 'App'},React.createElement('h1',null,'Does This Works?'))
