d5ac9358def8689ca7c0857e8776ee92265faa31
arjun <arjun.ghimire8386@gmail.com>
2020-01-06 20:30:49 +0545

    Dynamic content

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
     <div className="App">
       <h2>Hi ! I am React App </h2>
       <Person />
+      <Person />
+      <Person />
     </div>
   );
   //return React.createElement('div',{className: 'App'},React.createElement('h1',null,'Does This Works?'))
